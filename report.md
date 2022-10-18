89a5d52bcf812bb401f9ad2b7b905b1938b699b6
am_6733 <anton6733@gmail.com>
2022-10-18 12:35:14 +0300

    ...bye, java

--- report.md
+++ report.md
@@ -1,5 +1,4 @@
 |platform|1|2|3|
 |:-|:-|:-|:-|
-|java|[offtop1]()|[offtop2]()|[offtop3]()|
 |py|[offtop4]()|[offtop5]()|[offtop6]()|
 |c#|[topic1]()|[topic2]()|[topic3]()|
---
+++ task_c-sharp/legacy_array_randomized.cs
@@ -0,0 +1,59 @@
+// <summary>Напишите программу, которая задаёт массив из N 3лементов и выводит их на экран.  
+// </summary>
+// * класс рандомаррэй?
+// * Старт.
+// * Рандомим массив.// можно взять формулу с практики.
+// * Печатаем за один for
+// </details>
+using static System.Console;
+class array_randomized
+{
+    int size,lower_limit,upper_limit;
+    int[] client;
+    public array_randomized(int size_i, int lower_limit_i, int upper_limit_i)
+    {
+        this.size=size_i;
+        this.lower_limit=lower_limit_i;
+        this.upper_limit=upper_limit_i;
+        this.client=calculation();
+    }
+    public dynamic calculation()
+    {
+        int[] output= new int[this.size];
+        Random _gen = new Random();
+        for (int i = 0; i < size; i++)
+        {
+            output[i]=_gen.Next(lower_limit,upper_limit);
+        }
+        return output;
+    }
+    public dynamic print_result()
+    {
+        for (int step = 0; step < this.client.Length; step++)
+        {
+            if (step==0)
+            {
+                Write($"[{this.client[step]}, ");
+            }
+            else if(step==this.client.Length-1)
+            {
+                Write($"{this.client[step]}]");
+            }
+            else
+            {
+                Write($"{this.client[step]}, ");
+            }
+        }
+    return null;
+    }
+    public dynamic return_string()
+    {
+        string output=$"[{String.Join(", ",this.client)}]";
+        return output;
+    }
+    public dynamic represent()
+    {
+        WriteLine($"Рандомим массив размером {this.size}элементов(size_i), с пределами {this.lower_limit}-{this.upper_limit}");
+        return null;
+    }
+}
